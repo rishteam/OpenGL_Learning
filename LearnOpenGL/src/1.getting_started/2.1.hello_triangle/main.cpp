f5cad9a09f9eb827b415bddc9cbd808c88ab441d
roy4801 <a82611141@gmail.com>
2020-06-27 21:53:31 +0800

    Add 2.1 triangle

---
+++ LearnOpenGL/src/1.getting_started/2.1.hello_triangle/main.cpp
@@ -0,0 +1,173 @@
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+#include <glad/glad.h>
+#include <SFML/OpenGL.hpp>
+
+#include <imgui.h>
+#include <imgui-SFML.h>
+
+void init()
+{
+    setvbuf(stdin, nullptr, _IONBF, 0);
+    setvbuf(stdout, nullptr, _IONBF, 0);
+}
+// Vertex Shader
+const char *vertexShaderSource = R"glsl(
+#version 450 core
+layout (location = 0) in vec3 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+)glsl";
+// Fragment Shader
+const char* fragmentShaderSoucre = R"glsl(
+#version 450 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+)glsl";
+
+uint32_t LoadShader(GLenum type, const char* src)
+{
+    uint32_t shader;
+    shader = glCreateShader(type);
+    glShaderSource(shader, 1, &src, nullptr);
+    glCompileShader(shader);
+
+    int success;
+    char log[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(shader, 512, nullptr, log);
+        printf("Error Shader %s compile error\n%s\n", type == GL_VERTEX_SHADER ? "Vertex" : "Fragment", log);
+        return 0;
+    }
+    return shader;
+}
+
+uint32_t LinkShaderProgram(uint32_t vertex, uint32_t fragment)
+{
+    uint32_t program = glCreateProgram();
+    glAttachShader(program, vertex);
+    glAttachShader(program, fragment);
+    glLinkProgram(program);
+    //
+    int success;
+    char log[512];
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        glGetProgramInfoLog(program, 512, nullptr, log);
+        printf("Error Shader Linking error\n%s\n", log);
+        return 0;
+    }
+    return program;
+}
+
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, sf::ContextSettings(
+        24, // depthBits
+        8,  // stencilBits
+        4,  // antialiasingLevel
+        4,  // majorVersion
+        4   // minorVersion
+    ));
+    window.setVerticalSyncEnabled(true);
+    // Load OpenGL functions using glad
+    if (!gladLoadGL())
+    {
+        printf("Something went wrong!\n");
+        exit(-1);
+    }
+    printf("OpenGL %s, GLSL %s\n", glGetString(GL_VERSION), glGetString(GL_SHADING_LANGUAGE_VERSION));
+    ImGui::SFML::Init(window);
+    window.setActive(true);
+    init();
+
+    // Load shader
+    uint32_t vertexShader = LoadShader(GL_VERTEX_SHADER, vertexShaderSource);
+    uint32_t fragmentShader = LoadShader(GL_FRAGMENT_SHADER, fragmentShaderSoucre);
+    uint32_t program;
+    if(vertexShader && fragmentShader)
+    {
+        program = LinkShaderProgram(vertexShader, fragmentShader);
+        glUseProgram(program);
+    }
+    else
+    {
+        if(vertexShader) glDeleteShader(vertexShader);
+        if(fragmentShader) glDeleteShader(fragmentShader);
+        return 1;
+    }
+    // Load vertices
+    // VAO
+    uint32_t vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    // VBO
+    uint32_t vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(
+        0,                   // Location
+        3,                   // size
+        GL_FLOAT,            // type
+        GL_FALSE,            // normalize?
+        3 * sizeof(float),   // stride
+        nullptr              // offsets
+    );
+    glEnableVertexAttribArray(0);
+
+    sf::Clock deltaClock;
+    bool running = true;
+    while (running)
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            ImGui::SFML::ProcessEvent(event);
+            if (event.type == sf::Event::Closed)
+                running = false;
+            else if (event.type == sf::Event::Resized)
+                glViewport(0, 0, event.size.width, event.size.height);
+        }
+        // ImGui Update
+        ImGui::SFML::Update(window, deltaClock.restart());
+        ImGui::Begin("hello, world");
+        ImGui::End();
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glBindVertexArray(0);
+        window.pushGLStates();
+        {
+            ImGui::SFML::Render(window);
+        }
+        window.popGLStates();
+        window.display();
+    }
+
+    // release resources...
+
+    return 0;
+}
