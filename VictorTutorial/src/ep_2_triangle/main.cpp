6f6c5a25f3a1d5f5ac824232155723593dc22df7
roy4801 <a82611141@gmail.com>
2022-02-01 06:05:56 +0800

    Add ep2 triangle
    VAO, VBO

--- VictorTutorial/src/ep_2_triangle/main.cpp
+++ VictorTutorial/src/ep_2_triangle/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <cmath>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+const char *vsSource = R"glsl(
+#version 460 core
+layout (location = 0) in vec3 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+)glsl";
+
+const char *fsSource = R"glsl(
+#version 460 core
+
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);
+}
+)glsl";
+
+GLfloat vertices[] =
+{
+        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+};
 
 int main()
 {
@@ -21,14 +50,59 @@ int main()
     gladLoadGL();
 
     glViewport(0, 0, 800, 600);
-    glClearColor(1.f, 0.5f, 0.5f, 1.f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
+
+    /* Load and create the shaders */
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vsSource, NULL);
+    glCompileShader(vertexShader);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fsSource, NULL);
+    glCompileShader(fragmentShader);
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    /* Create VAO, VBO */
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO); // BEFORE VBO
+    glBindVertexArray(VAO);
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    /* Setting Vertex Attribute Array */
+    glVertexAttribPointer(0,   // attribute index
+      3,                       // Number of component (here is vec3)
+      GL_FLOAT,                // Type of each component
+      GL_FALSE,                // Is normalized
+      3 * sizeof(float),       // Stride (Offset to the next group of the attribute)
+      (void*)0);               // Offset to the first attribute
+    glEnableVertexAttribArray(0);
+
+    /* Unbind things */
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
     while(!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
+        //
+        glClearColor(1.f, 0.5f, 0.5f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
     }
+
+    glDeleteShader(shaderProgram);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
     glfwDestroyWindow(window);
     glfwTerminate();
