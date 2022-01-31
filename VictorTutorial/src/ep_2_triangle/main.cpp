87ac2308e908c32207a779294e699af7275d6ca9
roy4801 <a82611141@gmail.com>
2022-02-01 06:32:28 +0800

    fix format

--- VictorTutorial/src/ep_2_triangle/main.cpp
+++ VictorTutorial/src/ep_2_triangle/main.cpp
@@ -26,9 +26,9 @@ void main()
 
 GLfloat vertices[] =
 {
-        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+    -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,     // Lower left corner
+    0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,      // Lower right corner
+    0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f    // Upper corner
 };
 
 int main()
